93dfb93136198eeb3eed527ae3bbc53f48d160d6
LivioAra <assislivio@gmail.com>
2021-06-11 12:11:06 -0300

    Initial commit

---
+++ app/src/main/java/matc89/exercicio1/exercicio01/MainActivity.java
@@ -0,0 +1,56 @@
+package matc89.exercicio1.exercicio01;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText campoNome;
+    private TextView campoMensagem;
+    private String nome;
+    static final String CHAVE_NOME = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        campoNome = findViewById(R.id.editNome);
+        campoMensagem = findViewById(R.id.labelMensagem);
+
+    }
+
+    public void cumprimentar(View view){
+
+        nome =campoNome.getText().toString();
+        campoMensagem.setText("Alô: " + nome);
+
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        // Salve todos seus dados aqui, lembrando que se for objeto é preciso ser serializable ou de preferencia parcelable
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(CHAVE_NOME, nome);
+        //Toast.makeText(this, nome, Toast.LENGTH_SHORT).show();
+        // Sempre chamar a super classe depois de salvar as instancias
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        nome= savedInstanceState.getString(CHAVE_NOME,"Alô:" );
+        campoMensagem.setText("Alô: " +nome);
+    }
+
+
+
+}
